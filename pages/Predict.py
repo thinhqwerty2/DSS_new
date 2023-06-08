9a9ab42b9fac4524e8e9badeeb53d17c409b4a1c
Thinhthon <thinhqwerty2@gmail.com>
2023-06-08 16:48:35 +0700

    Alpha 1.6

--- pages/Predict.py
+++ pages/Predict.py
@@ -25,7 +25,7 @@ chosen_product = st.selectbox('Chọn sản phẩm',stv['item_id'])
 dataset=stv.query(f'item_id=="{chosen_product}"')
 train_dataset = dataset[d_cols[-100:-30]].squeeze()
 val_dataset = dataset[d_cols[-30:]].squeeze()
-tab0, tab1, tab2, tab3 = st.tabs(["BaseLine", "Moving average", "SARIMAX", "So sánh"])
+tab0, tab1, tab2, tab3 = st.tabs(["Baseline", "Moving average", "SARIMAX", "So sánh"])
 with tab0:
     try:
         predictions = []
@@ -38,115 +38,73 @@ with tab0:
         # error_naive = np.linalg.norm(predictions[:3] - val_dataset.values[:3]) / len(predictions)
         pred_1 = predictions
 
-        fig = make_subplots(rows=1, cols=1)
+        fig0 = make_subplots(rows=1, cols=1)
 
-        fig.add_trace(
+        fig0.add_trace(
             go.Scatter(x=np.arange(70), mode='lines', y=train_dataset, marker=dict(color="dodgerblue"),
                        name="Train"),
             row=1, col=1
         )
 
-        fig.add_trace(
+        fig0.add_trace(
             go.Scatter(x=np.arange(70, 100), y=val_dataset, mode='lines', marker=dict(color="darkorange"),
                        name="Val"),
             row=1, col=1
         )
 
-        fig.add_trace(
+        fig0.add_trace(
             go.Scatter(x=np.arange(70, 100), y=pred_1, mode='lines', marker=dict(color="seagreen"),
                        name="Pred"),
             row=1, col=1
         )
 
 
-        fig.update_layout(height=400, width=800, title_text="Baseline")
-        st.plotly_chart(fig)
+        fig0.update_layout(height=400, width=800)
+        st.plotly_chart(fig0)
     except:
         pass
 with tab1:
-    try:
+
         predictions = []
-        for i in range(len(val_dataset.columns)):
+        for i in range(len(val_dataset)):
             if i == 0:
-                predictions.append(np.mean(train_dataset[train_dataset.columns[-30:]].values, axis=1))
+                predictions.append(np.mean(train_dataset[-30:]))
             if i < 31 and i > 0:
-                predictions.append(0.5 * (np.mean(train_dataset[train_dataset.columns[-30 + i:]].values, axis=1) + np.mean(predictions[:i], axis=0)))
+                predictions.append(0.5 * (np.mean(train_dataset[-30 + i:]) + np.mean(predictions[:i], axis=0)))
             if i > 31:
-                predictions.append(np.mean([predictions[:i]], axis=1))
+                predictions.append(np.mean([predictions[:i]]))
 
-        predictions = np.transpose(np.array([row.tolist() for row in predictions]))
-        error_avg = np.linalg.norm(predictions[:3] - val_dataset.values[:3]) / len(predictions[0])
+        # predictions = np.transpose(np.array([row.tolist() for row in predictions]))
+        # error_avg = np.linalg.norm(predictions[:3] - val_dataset.values[:3]) / len(predictions[0])
 
 
-        pred_1 = predictions[0]
-        pred_2 = predictions[1]
-        pred_3 = predictions[2]
+        fig1 = make_subplots(rows=1, cols=1)
 
-        fig = make_subplots(rows=3, cols=1)
+        pred_1 = predictions
 
-        pred_1 = predictions[0]
-        pred_2 = predictions[1]
-        pred_3 = predictions[2]
-
-        fig = make_subplots(rows=3, cols=1)
-
-        fig.add_trace(
-            go.Scatter(x=np.arange(70), mode='lines', y=train_dataset.loc[0].values, marker=dict(color="dodgerblue"),
+        fig1.add_trace(
+            go.Scatter(x=np.arange(70), mode='lines', y=train_dataset, marker=dict(color="dodgerblue"),
                        name="Train"),
             row=1, col=1
         )
 
-        fig.add_trace(
-            go.Scatter(x=np.arange(70, 100), y=val_dataset.loc[0].values, mode='lines', marker=dict(color="darkorange"),
+        fig1.add_trace(
+            go.Scatter(x=np.arange(70, 100), y=val_dataset, mode='lines', marker=dict(color="darkorange"),
                        name="Val"),
             row=1, col=1
         )
 
-        fig.add_trace(
+        fig1.add_trace(
             go.Scatter(x=np.arange(70, 100), y=pred_1, mode='lines', marker=dict(color="seagreen"),
                        name="Pred"),
             row=1, col=1
         )
 
-        fig.add_trace(
-            go.Scatter(x=np.arange(70), mode='lines', y=train_dataset.loc[1].values, marker=dict(color="dodgerblue"),
-                       showlegend=False),
-            row=2, col=1
-        )
 
-        fig.add_trace(
-            go.Scatter(x=np.arange(70, 100), y=val_dataset.loc[1].values, mode='lines', marker=dict(color="darkorange"),
-                       showlegend=False),
-            row=2, col=1
-        )
 
-        fig.add_trace(
-            go.Scatter(x=np.arange(70, 100), y=pred_2, mode='lines', marker=dict(color="seagreen"), showlegend=False,
-                       name="Denoised signal"),
-            row=2, col=1
-        )
+        fig1.update_layout(height=400, width=800)
 
-        fig.add_trace(
-            go.Scatter(x=np.arange(70), mode='lines', y=train_dataset.loc[2].values, marker=dict(color="dodgerblue"),
-                       showlegend=False),
-            row=3, col=1
-        )
-
-        fig.add_trace(
-            go.Scatter(x=np.arange(70, 100), y=val_dataset.loc[2].values, mode='lines', marker=dict(color="darkorange"),
-                       showlegend=False),
-            row=3, col=1
-        )
-
-        fig.add_trace(
-            go.Scatter(x=np.arange(70, 100), y=pred_3, mode='lines', marker=dict(color="seagreen"), showlegend=False,
-                       name="Denoised signal"),
-            row=3, col=1
-        )
-
-        fig.update_layout(height=1200, width=800, title_text="Moving average")
-
-        st.plotly_chart(fig)
+        st.plotly_chart(fig1)
 
         # ss = pd.read_csv(f'{INPUT_DIR}/sample_submission.csv')
         #
@@ -158,8 +116,7 @@ with tab1:
         # data = ss.to_csv().encode()
         # st.download_button(label="Tải xuống", data=data, file_name='predict_30day.csv',
         #                    mime='text/csv', )
-    except:
-        pass
+
 # %% md
 # ARIMA
 
